f4bc41bfc70bb70a9bbb144147f31f9b62c86e6a
Behzad Barzin <behzad_barzin@live.com>
2024-06-27 01:57:39 +0330

    Adds Stories

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-x-scroll text-sm no-scrollbar">
+      {/* Stories Container */}
+      <div className="flex gap-8 w-max">
+        {/* Single Story */}
+        {Array(20)
+          .join()
+          .split(",")
+          .map((_, i) => (
+            <div
+              key={i}
+              className="flex flex-col items-center gap-2 cursor-pointer"
+            >
+              <Image
+                src="/sample-story.jpg"
+                alt=""
+                width={80}
+                height={80}
+                className="w-20 h-20 rounded-full ring-2"
+              />
+              <span className="font-medium ">John Doe</span>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Stories;

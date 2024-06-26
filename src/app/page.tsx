285129dde4d401115e15b689f55f8b355d5ffbb4
Behzad Barzin <behzad_barzin@live.com>
2024-06-27 01:45:26 +0330

    Setup Home structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import AddPost from "@/components/AddPost";
+import Feed from "@/components/Feed";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Stories from "@/components/Stories";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-7xl font-extrabold">Hello World!</h1>
+    <main className="flex gap-6 pt-6">
+      {/* Left */}
+      <div className="hidden xl:block xl:w-[20%]">
+        <LeftSidebar />
+      </div>
+      {/* Center */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="hidden lg:block lg:w-[30%]">
+        <RightSidebar />
+      </div>
     </main>
   );
 }

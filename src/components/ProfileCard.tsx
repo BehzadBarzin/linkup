410b202e5169e876b893c26d8f43999bfc10dfd4
Behzad Barzin <behzad_barzin@live.com>
2024-06-27 14:29:42 +0330

    Add real data to ProfileCard

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,22 +1,46 @@
 import React, { FC } from "react";
 import Card from "./Card";
 import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/utils/db";
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+  // ---------------------------------------------------------------------------
+  // Get currently logged in user from Clerk
+  const { userId } = auth();
+
+  if (!userId) return null;
+  // ---------------------------------------------------------------------------
+  // Get user info and number of its followers from DB
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+
+  if (!user) return null;
+  // ---------------------------------------------------------------------------
   return (
     <Card className="flex flex-col gap-6">
       {/* User Images */}
       <div className="h-20 relative">
         {/* Cover Image */}
         <Image
-          src="/sample-post.jpg"
+          src={user.cover || "/noCover.png"}
           alt=""
           fill
           className="rounded-lg object-cover"
         />
         {/* Avatar */}
         <Image
-          src="/sample-avatar.jpg"
+          src={user.avatar || "/noAvatar.png"}
           alt=""
           width={48}
           height={48}
@@ -26,7 +50,7 @@ const ProfileCard = () => {
       {/* Info */}
       <div className="my-4 h-24 flex flex-col gap-2 items-center">
         {/* User name */}
-        <span className="font-semibold">John Doe</span>
+        <span className="font-semibold">{`${user.name} ${user.surname}`}</span>
         {/* Followers */}
         <div className="flex items-center gap-4 my-2">
           <div className="flex gap-1">
@@ -45,7 +69,9 @@ const ProfileCard = () => {
               ))}
           </div>
           {/* Count */}
-          <span className="text-xs text-gray-500">843&nbsp;Followers</span>
+          <span className="text-xs text-gray-500">
+            {user._count.followers}&nbsp;Followers
+          </span>
         </div>
         {/* Button */}
         <button className="bg-teal-500 hover:bg-teal-800 transition-all text-white p-2 text-sm rounded-lg">

32264c4e036991bee7b4d089992924659c8e8578
Behzad Barzin <behzad_barzin@live.com>
2024-06-27 05:29:05 +0330

    Setup Profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Feed from "@/components/Feed";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
-const Profile = () => {
-  return <div>Profile</div>;
-};
-
-export default Profile;
+export default function Profile() {
+  return (
+    <main className="flex gap-6 pt-6">
+      {/* Left */}
+      <div className="hidden xl:block xl:w-[20%]">
+        <LeftSidebar />
+      </div>
+      {/* Center */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Feed />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="hidden lg:block lg:w-[30%]">
+        <RightSidebar />
+      </div>
+    </main>
+  );
+}
